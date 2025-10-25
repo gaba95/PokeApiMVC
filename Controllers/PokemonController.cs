f7eaa3a312c0bcbb5d8d4eccd907eed23da06b76
Gabriela <gabyg488@gmail.com>
2025-10-24 19:10:04 -0600

    primera carga

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json.Linq;
+using PokeApiMvc.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace PokeApiMvc.Controllers
+{
+    public class PokemonController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(string pokemonName)
+        {
+            if (string.IsNullOrEmpty(pokemonName))
+            {
+                ViewBag.Error = "Por favor, ingresa un nombre de Pokémon.";
+                return View();
+            }
+
+            string apiUrl = $"https://pokeapi.co/api/v2/pokemon/{pokemonName.ToLower()}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        JObject data = JObject.Parse(json);
+
+                        var pokemon = new Pokemon
+                        {
+                            Name = data["name"]?.ToString(),
+                            Height = (double)data["height"] / 10,
+                            Weight = (double)data["weight"] / 10,
+                            ImageUrl = data["sprites"]?["front_default"]?.ToString(),
+                            Types = new List<string>()
+                        };
+
+                        foreach (var type in data["types"])
+                        {
+                            pokemon.Types.Add(type["type"]?["name"]?.ToString());
+                        }
+
+                        return View(pokemon);
+                    }
+                    else
+                    {
+                        ViewBag.Error = $"No se encontró el Pokémon '{pokemonName}'.";
+                        return View();
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    ViewBag.Error = $"Error al conectar con la API: {ex.Message}";
+                    return View();
+                }
+            }
+        }
+    }
+}
---
+++ Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PokeApiMvc.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string ImageUrl { get; set; }
+        public List<string> Types { get; set; }
+    }
+}
